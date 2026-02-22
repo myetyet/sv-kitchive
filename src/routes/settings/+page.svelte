a07a3daf5a1ea60ae374650477e0010d3d4c36f1
myetyet <myetyet@126.com>
2026-02-22 14:33:29 +0800

    impr: color spelling

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -18,19 +18,19 @@
 <div class="min-h-full flex items-center justify-center p-6">
     <div class="grid grid-cols-2 gap-8">
         <button
-            onclick={() => isDarkMode = !isDarkMode}
+            onclick={() => (isDarkMode = !isDarkMode)}
             class="btn gap-8 flex flex-col items-center justify-center p-8 preset-outlined-surface-500 shadow-xl border-2! rounded-2xl transition-transform active:scale-97" title="切换显示模式">
             {#if isDarkMode}
-                <MoonIcon class="size-12" color="oklch(86.9% 0.022 252.894)" /><!-- slate-300 -->
+                <MoonIcon class="size-12 stroke-slate-300" />
             {:else}
-                <SunIcon class="size-12" color="oklch(75% 0.183 55.934)" /><!-- orange-400 -->
+                <SunIcon class="size-12 stroke-orange-400" />
             {/if}
             <span class="text-xl font-semibold text-center">{isDarkMode ? '暗' : '亮'}色模式</span>
         </button>
         <button
             onclick={() => { supabase.forceLogout(true); }}
             class="btn gap-8 flex flex-col items-center justify-center p-8 preset-outlined-surface-500 shadow-xl border-2! rounded-2xl transition-transform active:scale-97" title="退出登录">
-            <LogOutIcon class="size-12" color={isDarkMode ? 'oklch(80.9% 0.105 251.813)' : 'oklch(62.3% 0.214 259.815)'} /><!-- blue-300 & blue-500 -->
+            <LogOutIcon class="size-12 stroke-blue-500 dark:stroke-blue-300" />
             <span class="text-xl font-semibold text-center">退出登录</span>
         </button>
     </div>
